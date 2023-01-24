5e390c31f0552700500b342bd62bc5c8c1283944
Alva Jufinto <yeahbruh199@gmail.com>
2023-01-24 22:30:03 +0700

    successfully added tailwind to next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        asdasd
-      </main>
+      <p className="text-3xl font-bold ">
+        Hello world!
+      </p>
     </>
   )
 }
