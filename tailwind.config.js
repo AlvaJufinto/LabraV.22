bb19bb0082ce462e1f9056902f69e44311eb2178
AlvaJufinto <yeahbruh199@gmail.com>
2023-01-25 07:56:00 +0700

    ADD: components and setting up tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      'purple': '#8C30F5',
+      'color': '#FFFFFF',
+      'black': '#000000',
+      'grey': '#ECECF4',
+    },
     extend: {},
   },
   plugins: [],
