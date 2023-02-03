0078c4bb81efa764aa12df9c57405097aad2e5f2
AlvaJufinto <yeahbruh199@gmail.com>
2023-02-03 09:45:43 +0700

    ADD: customizing bubbles for slider

--- src/components/organisms/Slider.tsx
+++ src/components/organisms/Slider.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Swiper, SwiperSlide, useSwiper  } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 
@@ -8,33 +7,21 @@ import GlobalContainer from "@/components/templates/GlobalContainer";
 import { sliderContent } from "@/data";
 
 import "swiper/css";
+import 'swiper/css/pagination';
+
 
 export default function Slider() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const swiperInstance = useSwiper();
 
-  const onSwiperSlideChange: any = (swiper: any) => {
-    setActiveIndex(swiper.activeIndex);
-  };  
-  
-  const onButtonClick = (index: number) => {
-    setActiveIndex(index);
-    // if (swiperInstance) {
-    //   swiperInstance.slideTo(index);
-    // }
-  };
   return (
     <Swiper 
       className="w-[100vw] h-[45vw] min-h-[500px]" 
+      modules={[Autoplay, Navigation, Pagination]}
       id="Overview" 
-      navigation={true}
-      initialSlide={activeIndex}
+      pagination={{ clickable: true }}
       autoplay={{
         delay: 2500,
         disableOnInteraction: false,
-      }}
-      modules={[Autoplay]}
-      onSlideChange={onSwiperSlideChange}>
+      }}>
         {sliderContent.map((slider, i) => (
           <SwiperSlide key={`${slider}-${i}`} style={{
             backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0)), url(${slider.img.src})`
@@ -46,11 +33,6 @@ export default function Slider() {
             </GlobalContainer>
           </SwiperSlide>
         ))}
-        <div className="flex gap-[20px] absolute z-10 bottom-[4%] left-[50vw] -translate-x-[50%]">
-          {Array(sliderContent.length).fill("").map((arr, i) => (
-            <div key={`arr-${i}`} onClick={() => onButtonClick(i)} className={`rounded-full h-[20px] w-[20px] bg-white cursor-pointer ${activeIndex === i ? '' : 'opacity-50'}`}></div>
-          ))}
-        </div>
       </Swiper>
   )
 }
