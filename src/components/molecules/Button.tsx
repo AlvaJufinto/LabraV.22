adf23803d0aad223efb691d6231a36e1b656796f
AlvaJufinto <yeahbruh199@gmail.com>
2023-02-03 10:07:54 +0700

    ADD: Mobile Nav fix

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 
 interface Props {
     children: ReactNode,
+    addClassName: string,
 }
 
-const Button = ({ children }: Props) => {
+const Button = ({ children, addClassName }: Props) => {
     return (
-        <button className="btn select-none bg-purple text-white py-[10px] px-[20px] text-[14px] font-medium rounded-[5px]">
+        <button className={`btn select-none bg-purple text-white py-[10px] px-[20px] text-[14px] font-medium rounded-[5px] ${addClassName}`}>
             {children}
         </button>
     )
--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -24,20 +24,16 @@ const MobileNavbar = ({ setIsShowNav, isShowNav }: any) => {
     return (
         <div className={`${isShowNav ? "translate-x-[0vw]" : "translate-x-[100vw] " } transition-all duration-300 top-0 left-0 fixed h-[100vh] w-[100vw] bg-grey p-[20px] z-50`}>
             <div className="flex flex-col justify-between h-[100%]">
-                <Image src={CloseIcon} alt="CLoseIcon"  onClick={() => setIsShowNav(false)} />
+                <Image src={CloseIcon} alt="CloseIcon" onClick={() => setIsShowNav(false)} />
                 <div className="flex flex-col gap-[20px]">
                     {links.map(link => (
                         <div key={`${link}-link`} onClick={() => toLink(link)}>
                             <Text textType={TextType.SubTitle} addClassName="text-black text-right font-regular">{link}</Text>
                         </div>    
                     ))} 
-                    <LinkComponent href="#ContactUs" addClassName="text-right">
-                        <Button >
-                            <div onClick={() => toLink("ContactUs")}>
-                                <Text textType={TextType.SubTitle}>Contact Us</Text>
-                            </div>
-                        </Button>
-                    </LinkComponent>
+                    <div onClick={() => toLink("ContactUs")} className="bg-purple text-white w-[150px] text-center py-[10px] rounded-[6px] self-end	">
+                        <Text textType={TextType.SubTitle}>Contact Us</Text>
+                    </div>
                 </div>
                 <Text textType={TextType.SubTitle} addClassName="font-bold">Labra <span className="purple">V.22</span></Text>
             </div>
@@ -67,31 +63,28 @@ const Navbar = () => {
     return (
         <>
             <MobileNavbar setIsShowNav={setIsShowNav} isShowNav={isShowNav} />
-            {
-                !isShowNav &&
-                <nav className={`flex mt-0 fixed z-10 h-[70px] ${isScrolled ? 'bg-white' : ''} transition duration-300 ease-in-out`}>
-                    <GlobalContainer addClassName="flex justify-between items-center">
-                        {  
-                            // isShowNav &&
-                            <Text textType={TextType.Regular} addClassName={`text-white font-extrabold ${isScrolled ? 'text-black' : ''}`}>LABRA <span className="purple">V.22</span></Text>
-                        } 
-                        <div className="flex gap-[30px] items-center ${isScrolled ? 'text-black' : ''} sm:hidden">
-                            {links.map(link => (
-                                <LinkComponent key={`${link}-link`} href={`#${link}`} addClassName={`${isScrolled ? 'text-black' : 'text-white'}`}>{link}</LinkComponent>
-                            ))}
-                            <LinkComponent href="#ContactUs"><Button>Contact Us</Button></LinkComponent>
+            <nav className={`flex mt-0 fixed z-10 h-[70px] ${isScrolled ? 'bg-white' : ''} transition duration-300 ease-in-out`}>
+                <GlobalContainer addClassName="flex justify-between items-center">
+                    {  
+                        // isShowNav &&
+                        <Text textType={TextType.Regular} addClassName={`text-white font-extrabold ${isScrolled ? 'text-black' : ''}`}>LABRA <span className="purple">V.22</span></Text>
+                    } 
+                    <div className="flex gap-[30px] items-center ${isScrolled ? 'text-black' : ''} sm:hidden">
+                        {links.map(link => (
+                            <LinkComponent key={`${link}-link`} href={`#${link}`} addClassName={`${isScrolled ? 'text-black' : 'text-white'}`}>{link}</LinkComponent>
+                        ))}
+                        <LinkComponent href="#ContactUs"><Button>Contact Us</Button></LinkComponent>
+                    </div>
+                    {
+                        !isShowNav && 
+                        <div className="hidden sm:flex flex-col gap-[5px] w-[30px]" onClick={() => setIsShowNav(!isShowNav)}>
+                            <div className={`h-[2px] w-[100%] ${isScrolled ? 'bg-black' : 'bg-white'}`}></div>
+                            <div className={`h-[2px] w-[100%] ${isScrolled ? 'bg-black' : 'bg-white'}`}></div>
+                            <div className={`h-[2px] w-[100%] ${isScrolled ? 'bg-black' : 'bg-white'}`}></div>
                         </div>
-                        {
-                            !isShowNav && 
-                            <div className="hidden sm:flex flex-col gap-[5px] w-[30px]" onClick={() => setIsShowNav(!isShowNav)}>
-                                <div className={`h-[2px] w-[100%] ${isScrolled ? 'bg-black' : 'bg-white'}`}></div>
-                                <div className={`h-[2px] w-[100%] ${isScrolled ? 'bg-black' : 'bg-white'}`}></div>
-                                <div className={`h-[2px] w-[100%] ${isScrolled ? 'bg-black' : 'bg-white'}`}></div>
-                            </div>
-                        }
-                    </GlobalContainer>
-                </nav>
-            }
+                    }
+                </GlobalContainer>
+            </nav>
         </>
     )
 }
